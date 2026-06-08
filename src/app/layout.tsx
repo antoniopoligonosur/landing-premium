d2b6b97fa811e31e79c510d0780b001c2f5eee00
Antonio Mateos <principalemed@gmail.com>
2026-06-08 22:11:44 +0200

    agrega favicon web

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,9 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "https://marcelosschoolsevilla.com",
   },
+  icons: {
+    icon: "/favicon.jpeg",
+  },
 };
 
 const jsonLd = {
